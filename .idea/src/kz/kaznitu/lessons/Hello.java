99da2e64204c3f2ce200784a36321eeb020a68e8
Bibol <ergesh.bibol@mail.ru>
2017-10-04 23:35:02 +0600

    Екінші коммит

--- .idea/src/kz/kaznitu/lessons/Hello.java
+++ .idea/src/kz/kaznitu/lessons/Hello.java
@@ -3,5 +3,7 @@ package kz.kaznitu.lessons;
 public class Hello {
     public static void main(String[]args){
         System.out.println("Hello Bibol");
+        System.out.println("Бұл зертханалық жұмыстың мақсаты");
+        System.out.println("GitHub -пен жұмыс жасап үйрену");
     }
 }
