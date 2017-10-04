09d89eb0bc7d710d43dc182f39d274860bd8cacb
Bibol <ergesh.bibol@mail.ru>
2017-10-05 00:16:16 +0600

    expe1 бұтағында Hello.java және MyMath.java файлдарына өзгерістер енгізілді

--- .idea/src/kz/kaznitu/lessons/MyMath.java
+++ .idea/src/kz/kaznitu/lessons/MyMath.java
@@ -7,4 +7,20 @@ public class MyMath {
       public static int kub (int a){
         return a*a*a;
       }
+      public static boolean isEvent(int a){
+          return (a%2==0);
+      }
+      public static int mx(int a, int b){
+          if(a>=b)
+              return a;
+          else
+              return b;
+
+      }
+      public static int min(int a,int b){
+          if(a>=b)
+              return b;
+          else
+              return a;
+      }
 }
